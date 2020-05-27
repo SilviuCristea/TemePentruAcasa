541788a14c852b2d31236bb6d982f2fe6a3690bd
alexmatei88 <58986113+alexmatei88@users.noreply.github.com>
2020-05-27 11:37:09 +0300

    Update OutOfRangeGrades.java

--- Students_23MAI2020/src/main/java/OutOfRangeGrades.java
+++ Students_23MAI2020/src/main/java/OutOfRangeGrades.java
@@ -1,4 +1,5 @@
 public class OutOfRangeGrades extends Exception{
+    // numele exceptiilor e bine sa se termine cu "Exception"
     public OutOfRangeGrades(String message){
         super(message);
     }
